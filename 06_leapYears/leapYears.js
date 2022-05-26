b3a1fa83211c25c8ee66edd3de1965fe53b24f9c
Fábio Vinícius Lima <limavfabio@gmail.com>
2022-05-26 20:57:55 -0300

    Complete sixth JS exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,37 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (divisibleByFourHundred(year) === true) {
+        return true;
+    } else if (divisibleByFour(year) === true && divisibleByHundred(year) === false) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
-};
 
+
+function divisibleByFour(num) {
+    if (num % 4 === 0) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function divisibleByHundred(num) {
+    if (num % 100 === 0) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function divisibleByFourHundred(num) {
+    if (num % 400 === 0) {
+        return true
+    } else {
+        return false
+    }
+} 
 // Do not edit below this line
 module.exports = leapYears;
