ac150f9ea3b032ed83853e433a2fa7c14dca6132
Fábio Vinícius Lima <limavfabio@gmail.com>
2022-05-26 15:31:41 -0300

    Complete third JS exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let splitString = string.split("");
+    splitString.reverse();
+    let result = splitString.join("");
+    return result;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+/* 
+create an array with each character or a string as an element
+reverse the array
+join the array as an string
+return the reverted string
+
+
+*/
